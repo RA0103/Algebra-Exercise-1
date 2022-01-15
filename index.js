62d6cac9668b80727d8abf2da58b3096baee4b63
ilyosbek <ilyosbek.inomjonov.1996@gmail.com>
2022-01-15 09:25:23 +0500

    done

---
+++ index.js
@@ -0,0 +1,31 @@
+// Create a function that takes two numbers as arguments and return their sum.
+
+// Examples
+// addition(3, 2) ➞ 5
+
+// addition(-3, -6) ➞ -9
+
+// addition(7, 3) ➞ 10
+
+// First Way
+
+// function addition(a, b) {
+//     return a + b
+//   }
+//   console.log(addition(3, 2));
+  
+// Second Way
+
+// function addition(a, b) {
+//     if (typeof a !== "number" || typeof b !== "number") {
+//         return "Sorry but you didn't pass two numbers.";
+//     }
+//     return a + b;
+// }
+// console.log(addition(5,7));
+
+// Third Way
+
+// const addition = (a, b) => a + b;
+// console.log(addition(5,7));
+
